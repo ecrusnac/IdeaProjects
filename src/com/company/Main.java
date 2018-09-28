20c1fc8f91bc460e42dd0ee05df75bd5681fbaa0
Ecaterina Rusnac <ecaterina.rusnac@endava.com>
2018-09-28 10:50:06 +0300

    Changing Main file from master

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	    System.out.println("Some text!");
-        System.out.println("Some other text without sense!");
+        System.out.println("Some other text without sense! definetely");
     }
 }
